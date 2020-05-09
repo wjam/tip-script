d5931424c361a7fa4bae55779c9f8a198c8b8849
Will May <will.j.may@gmail.com>
2020-05-09 16:02:36 +0100

    Restructure for ease of extension

---
+++ cmd/tip_script/handlers.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/apparentlymart/go-cidr/cidr"
+)
+
+var _ handler = &cidrHandler{}
+
+type cidrHandler struct {
+}
+
+func (h *cidrHandler) regex() *regexp.Regexp {
+	return regexp.MustCompile("^(\\d{1,3}(\\.\\d{1,3}(\\.\\d{1,3}(\\.\\d{1,3})?)?)?)/(\\d{1,3})$")
+}
+
+func (h *cidrHandler) handle(input string) ([]response, error) {
+	groups := h.regex().FindStringSubmatch(input)
+	missingOctets := 0
+	for _, group := range groups {
+		if group == "" {
+			missingOctets++
+		}
+	}
+	input = fmt.Sprintf("%s%s/%s", groups[1], strings.Repeat(".0", missingOctets), groups[5])
+
+	_, c, err := net.ParseCIDR(input)
+	if err != nil {
+		return nil, err
+	}
+
+	prefix, _ := c.Mask.Size()
+	start, end := cidr.AddressRange(c)
+
+	responses := []response{text(fmt.Sprintf("%s - %s", start, end))}
+
+	if subnet, exceeded := cidr.PreviousSubnet(c, prefix); !exceeded {
+		responses = append(responses, text(fmt.Sprintf("Previous: %s", subnet)))
+	}
+	if subnet, exceeded := cidr.NextSubnet(c, prefix); !exceeded {
+		responses = append(responses, text(fmt.Sprintf("Next: %s", subnet)))
+	}
+
+	return responses, nil
+}
+
+var _ handler = &epochHandler{}
+
+type epochHandler struct {
+}
+
+func (_ *epochHandler) regex() *regexp.Regexp {
+	return regexp.MustCompile("^\\d+$")
+}
+
+func (_ *epochHandler) handle(input string) ([]response, error) {
+	i, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	var t time.Time
+	switch {
+	case i < 100000000000: // seconds
+		t = time.Unix(i, 0)
+	case i < 100000000000000: // millis
+		t = time.Unix(0, i*1000000)
+	default: // nanos
+		t = time.Unix(0, i)
+	}
+
+	return []response{text(t.UTC().String())}, nil
+}
--- cmd/tip_script/main.go
+++ cmd/tip_script/main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
-	"net"
 	"os"
 	"regexp"
-	"strconv"
-	"time"
-
-	"github.com/apparentlymart/go-cidr/cidr"
 )
 
 type response struct {
@@ -19,8 +13,12 @@ type response struct {
 	Label string `json:"label,omitempty"`
 }
 
-var cidrRegex = regexp.MustCompile("^\\d{1,3}(\\.\\d{1,3}(\\.\\d{1,3}(\\.\\d{1,3})?)?)?/\\d{1,3}$")
-var epochNumber = regexp.MustCompile("^\\d+$")
+type handler interface {
+	regex() *regexp.Regexp
+	handle(input string) ([]response, error)
+}
+
+var handlers = []handler{&cidrHandler{}, &epochHandler{}}
 
 func main() {
 	responses, err := handleInput(os.Args[1])
@@ -35,70 +33,32 @@ func main() {
 }
 
 func handleInput(input string) ([]response, error) {
-	if cidrRegex.MatchString(input) {
-		response, err := handleCidr(input)
-		if err != nil {
-			log.Printf("Failed to parse CIDR: %v", err)
-		} else {
-			return response, nil
+	var responses []response
+	for _, h := range handlers {
+		if h.regex().MatchString(input) {
+			response, err := h.handle(input)
+			if err != nil {
+				log.Printf("Failed: %v", err)
+			} else {
+				responses = append(responses, response...)
+			}
 		}
-	}
-
-	if epochNumber.MatchString(input) {
-		response, err := handleEpoch(input)
-		if err != nil {
-			log.Printf("Failed to parse epoch number: %v", err)
-		} else {
-			return response, nil
-		}
-	}
-
-	return []response{}, nil
-}
-
-func handleCidr(input string) ([]response, error) {
-	_, c, err := net.ParseCIDR(input)
-	if err != nil {
-		return nil, err
-	}
-
-	prefix, _ := c.Mask.Size()
-	start, end := cidr.AddressRange(c)
-
-	responses := []response{text(fmt.Sprintf("%s - %s", start, end))}
-
-	if subnet, exceeded := cidr.PreviousSubnet(c, prefix); !exceeded {
-		responses = append(responses, text(fmt.Sprintf("Previous: %s", subnet)))
-	}
-	if subnet, exceeded := cidr.NextSubnet(c, prefix); !exceeded {
-		responses = append(responses, text(fmt.Sprintf("Next: %s", subnet)))
 	}
 
 	return responses, nil
-}
-
-func handleEpoch(input string) ([]response, error) {
-	i, err := strconv.ParseInt(input, 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	var t time.Time
-	switch {
-	case i < 100000000000: // seconds
-		t = time.Unix(i, 0)
-	case i < 100000000000000: // millis
-		t = time.Unix(0, i*1000000)
-	default: // nanos
-		t = time.Unix(0, i)
-	}
-
-	return []response{text(t.UTC().String())}, nil
 }
 
 func text(text string) response {
 	return response{
 		Type:  "text",
 		Value: text,
+	}
+}
+
+func link(text string, url string) response {
+	return response{
+		Type:  "url",
+		Value: url,
+		Label: text,
 	}
 }
